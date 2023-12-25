1d88df90cda5dd86ac7dcaf95f1572442aa4ec85
Sergey Okatov <sokatov@gmail.com>
2023-12-25 22:56:09 +0500

    m1l1 first app and test

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,21 +1,18 @@
 plugins {
-    kotlin("jvm") version "1.9.21"
+    kotlin("jvm") apply false
 }
 
 group = "com.otus.otuskotlin.marketplace"
-version = "1.0-SNAPSHOT"
+version = "0.0.1"
 
 repositories {
     mavenCentral()
 }
 
-dependencies {
-    testImplementation("org.jetbrains.kotlin:kotlin-test")
-}
-
-tasks.test {
-    useJUnitPlatform()
-}
-kotlin {
-    jvmToolchain(17)
+subprojects {
+    repositories {
+        mavenCentral()
+    }
+    group = rootProject.group
+    version = rootProject.version
 }
---
+++ m1l1-first/src/test/kotlin/FirstTest.kt
@@ -0,0 +1,9 @@
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class FirstTest {
+    @Test
+    fun firstTest() {
+        assertEquals(3, 1 + 2)
+    }
+}
