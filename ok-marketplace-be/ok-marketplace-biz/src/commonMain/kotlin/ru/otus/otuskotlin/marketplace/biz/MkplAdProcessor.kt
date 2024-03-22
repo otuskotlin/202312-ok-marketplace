a002910b71a07fc0b4ca318e3e02571cd14abfe2
Sergey Okatov <sokatov@gmail.com>
2024-03-22 11:02:22 +0500

    m5l1 - Spring : fixes

--- ok-marketplace-be/ok-marketplace-biz/src/commonMain/kotlin/ru/otus/otuskotlin/marketplace/biz/MkplAdProcessor.kt
+++ ok-marketplace-be/ok-marketplace-biz/src/commonMain/kotlin/ru/otus/otuskotlin/marketplace/biz/MkplAdProcessor.kt
@@ -2,6 +2,7 @@ package ru.otus.otuskotlin.marketplace.biz
 
 import ru.otus.otuskotlin.marketplace.common.MkplContext
 import ru.otus.otuskotlin.marketplace.common.MkplCorSettings
+import ru.otus.otuskotlin.marketplace.common.models.MkplDealSide
 import ru.otus.otuskotlin.marketplace.common.models.MkplState
 import ru.otus.otuskotlin.marketplace.stubs.MkplAdStub
 
@@ -9,6 +10,7 @@ import ru.otus.otuskotlin.marketplace.stubs.MkplAdStub
 class MkplAdProcessor(val corSettings: MkplCorSettings) {
     suspend fun exec(ctx: MkplContext) {
         ctx.adResponse = MkplAdStub.get()
+        ctx.adsResponse = MkplAdStub.prepareSearchList("ad search", MkplDealSide.DEMAND).toMutableList()
         ctx.state = MkplState.RUNNING
     }
 }
