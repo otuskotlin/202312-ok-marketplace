25236bed4911454992225eb1b763db1eccca5574
Sergey Okatov <sokatov@gmail.com>
2024-05-03 10:25:25 +0500

    m7l1 - Repository: Refining

--- ok-marketplace-be/ok-marketplace-repo-inmemory/src/commonMain/kotlin/AdRepoInMemory.kt
+++ ok-marketplace-be/ok-marketplace-repo-inmemory/src/commonMain/kotlin/AdRepoInMemory.kt
@@ -4,6 +4,7 @@ import com.benasher44.uuid.uuid4
 import io.github.reactivecircus.cache4k.Cache
 import kotlinx.coroutines.sync.Mutex
 import kotlinx.coroutines.sync.withLock
+import ru.otus.otuskotlin.marketplace.common.helpers.errorSystem
 import ru.otus.otuskotlin.marketplace.common.models.*
 import ru.otus.otuskotlin.marketplace.common.repo.*
 import ru.otus.otuskotlin.marketplace.repo.common.IRepoAdInitializable
@@ -13,7 +14,7 @@ import kotlin.time.Duration.Companion.minutes
 class AdRepoInMemory(
     ttl: Duration = 2.minutes,
     val randomUuid: () -> String = { uuid4().toString() },
-) : IRepoAd, IRepoAdInitializable {
+) : AdRepoBase(), IRepoAd, IRepoAdInitializable {
 
     private val mutex: Mutex = Mutex()
     private val cache = Cache.Builder<String, AdEntity>()
@@ -27,28 +28,32 @@ class AdRepoInMemory(
         ad
     }
 
-    override suspend fun createAd(rq: DbAdRequest): IDbAdResponse {
+    override suspend fun createAd(rq: DbAdRequest): IDbAdResponse = tryAdMethod {
         val key = randomUuid()
         val ad = rq.ad.copy(id = MkplAdId(key))
         val entity = AdEntity(ad)
-        cache.put(key, entity)
-        return DbAdResponseOk(ad)
+        mutex.withLock {
+            cache.put(key, entity)
+        }
+        DbAdResponseOk(ad)
     }
 
-    override suspend fun readAd(rq: DbAdIdRequest): IDbAdResponse {
-        val key = rq.id.takeIf { it != MkplAdId.NONE }?.asString() ?: return errorEmptyId
-        return cache.get(key)
-            ?.let {
-                DbAdResponseOk(it.toInternal())
-            } ?: errorNotFound(rq.id)
+    override suspend fun readAd(rq: DbAdIdRequest): IDbAdResponse = tryAdMethod {
+        val key = rq.id.takeIf { it != MkplAdId.NONE }?.asString() ?: return@tryAdMethod errorEmptyId
+        mutex.withLock {
+            cache.get(key)
+                ?.let {
+                    DbAdResponseOk(it.toInternal())
+                } ?: errorNotFound(rq.id)
+        }
     }
 
-    override suspend fun updateAd(rq: DbAdRequest): IDbAdResponse {
+    override suspend fun updateAd(rq: DbAdRequest): IDbAdResponse = tryAdMethod {
         val rqAd = rq.ad
-        val id = rqAd.id.takeIf { it != MkplAdId.NONE } ?: return errorEmptyId
+        val id = rqAd.id.takeIf { it != MkplAdId.NONE } ?: return@tryAdMethod errorEmptyId
         val key = id.asString()
 
-        return mutex.withLock {
+        mutex.withLock {
             val oldAd = cache.get(key)?.toInternal()
             when {
                 oldAd == null -> errorNotFound(id)
@@ -60,16 +65,14 @@ class AdRepoInMemory(
                 }
             }
         }
-
-
     }
 
 
-    override suspend fun deleteAd(rq: DbAdIdRequest): IDbAdResponse {
-        val id = rq.id.takeIf { it != MkplAdId.NONE } ?: return errorEmptyId
+    override suspend fun deleteAd(rq: DbAdIdRequest): IDbAdResponse = tryAdMethod {
+        val id = rq.id.takeIf { it != MkplAdId.NONE } ?: return@tryAdMethod errorEmptyId
         val key = id.asString()
 
-        return mutex.withLock {
+        mutex.withLock {
             val oldAd = cache.get(key)?.toInternal()
             when {
                 oldAd == null -> errorNotFound(id)
@@ -85,8 +88,8 @@ class AdRepoInMemory(
      * Поиск объявлений по фильтру
      * Если в фильтре не установлен какой-либо из параметров - по нему фильтрация не идет
      */
-    override suspend fun searchAd(rq: DbAdFilterRequest): IDbAdsResponse {
-        val result = cache.asMap().asSequence()
+    override suspend fun searchAd(rq: DbAdFilterRequest): IDbAdsResponse = tryAdsMethod {
+        val result: List<MkplAd> = cache.asMap().asSequence()
             .filter { entry ->
                 rq.ownerId.takeIf { it != MkplUserId.NONE }?.let {
                     it.asString() == entry.value.ownerId
@@ -104,6 +107,6 @@ class AdRepoInMemory(
             }
             .map { it.value.toInternal() }
             .toList()
-        return DbAdsResponseOk(result)
+        DbAdsResponseOk(result)
     }
 }
--- ok-marketplace-be/ok-marketplace-repo-inmemory/src/commonTest/kotlin/AdRepoInMemoryTest.kt
+++ ok-marketplace-be/ok-marketplace-repo-inmemory/src/commonTest/kotlin/AdRepoInMemoryTest.kt
@@ -4,7 +4,7 @@ import ru.otus.otuskotlin.marketplace.repo.inmemory.AdRepoInMemory
 
 class AdRepoInMemoryCreateTest : RepoAdCreateTest() {
     override val repo = AdRepoInitialized(
-        AdRepoInMemory(),
+        AdRepoInMemory(randomUuid = { uuidNew.asString() }),
         initObjects = initObjects,
     )
 }
